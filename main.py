5755d7b3cf3d5c7380dae21c3394c82376cba2e9
Black Security Team <79002336+BlackSecurityTeam@users.noreply.github.com>
2021-02-14 13:54:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import functions
 import os
 from modules import whoiis,ddns,googlemap,subdomain,plugins,admin_finder,site_info,port_scanner,http_header
 from pprint import pprint
+from sys import platform
 
 GREEN = functions.GREEN
 RED = functions.RED
@@ -9,8 +10,15 @@ BLUE = functions.BLUE
 RESET = functions.RESET
 WHITE = functions.WHITE
 
+def clear():
+    
+    if platform == "linux" or platform == "linux2":
+        os.system('clear')
+    elif platform == "win32":
+        os.system('cls')
+        
 while True :
-    os.system('cls' or 'clear')
+    clear()
     functions.Tools.banner(None)
     functions.Tools.options(None)
 
@@ -18,7 +26,7 @@ while True :
         
         num = input(f" {RED} [+] {WHITE} Enter a number from the list : {RESET}")
         if num == '1' : ######################### whois
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             whois = whoiis.Information(domain)
             res_domain = whois.Domain_info(domain)
@@ -29,7 +37,7 @@ while True :
             pprint(res_Server)
             input()
         elif num == '2' : ########################## Dns
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             Dns = ddns.Dns_Checker(domain)
             Dns.options()
@@ -53,30 +61,30 @@ while True :
         elif num == '3' : ############ Ip location
             googlemap.generate()
         elif num == '4' : ############# Subdomains
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             subdomain.Discover_Subdomains(domain,WHITE,BLUE,RESET,RED,GREEN)
             print(f" {BLUE} Discover Ended ... {RESET}")
             input()
         elif num == '5' : ############ WordPrss Plugins
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             plugins.Discover_Plugins(domain,BLUE,RESET,RED,GREEN)
             print(f" {BLUE} Discover Ended ... {RESET}")
             input()
         elif num == '6' : ########### Admin Finder  
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             admin_finder.admin_finder(domain,GREEN,RED,RESET)
             print(f" {BLUE} Discover Ended ... {RESET}")
             input()
         elif num == '7' : ############ Web Site Info
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             site_info.Fetch_info(domain,GREEN)
             input()
         elif num == '8' : ############ Port scanner
-            os.system('cls' or 'clear')
+            clear()
             ip = input(f" {RED} [**] {WHITE} Enter a ip :  {RESET}")
             rrange = input(f" {RED} [**] {WHITE} Enter a Range of port :  {RESET}")
             port_scanner = port_scanner.Portscanner(ip,rrange,GREEN,RED,RESET)
@@ -84,7 +92,7 @@ while True :
             print(f" {BLUE} Discover Ended ... {RESET}")
             input()  
         elif num == '9' : ############ Http Header
-            os.system('cls' or 'clear')
+            clear()
             domain = input(f" {RED} [**] {WHITE} Enter a Domain :  {RESET}")
             res = http_header.http_header(domain)
             print(f"{RED} Results : \n")
@@ -93,28 +101,7 @@ while True :
      
 
         
-    
-
-
-       
-
-        
-
-
-
-
-        
-
-       
-
-
-
-
-                        
-
-
-
-
+  
 
             
     except Exception as e:
